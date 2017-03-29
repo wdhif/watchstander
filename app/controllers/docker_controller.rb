6f171b09eb06e8c4dd33cc02fd044f77710565a6
Wassim DHIF <wassimdhif@gmail.com>
2017-03-29 18:07:55 +0200

    container actions added

--- app/controllers/docker_controller.rb
+++ app/controllers/docker_controller.rb
@@ -1,4 +1,10 @@
 class DockerController < ApplicationController
+
+  helper_method :start_container
+  helper_method :stop_container
+  helper_method :kill_container
+  helper_method :delete_container
+
   def dashboard
     @docker_version = Docker.version
     @docker_info = Docker.info
@@ -10,5 +16,33 @@ class DockerController < ApplicationController
 
   def images
     @images = Docker::Image.all
+  end
+
+  def start_container()
+    if params.has_key?(:id)
+      Docker::Container.get(params['id']).start
+      redirect_to '/containers'
+    end
+  end
+
+  def stop_container()
+    if params.has_key?(:id)
+      Docker::Container.get(params['id']).stop
+      redirect_to '/containers'
+    end
+  end
+
+  def kill_container()
+    if params.has_key?(:id)
+      Docker::Container.get(params['id']).kill
+      redirect_to '/containers'
+    end
+  end
+
+  def delete_container()
+    if params.has_key?(:id)
+      Docker::Container.get(params['id']).delete(:force => true)
+      redirect_to '/containers'
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   root 'docker#dashboard'
 
   get '/containers' => 'docker#containers'
+  post '/start_container' => 'docker#start_container', as: :start_container
+  post '/stop_container' => 'docker#stop_container', as: :stop_container
+  post '/kill_container' => 'docker#kill_container', as: :kill_container
+  post '/delete_container' => 'docker#delete_container', as: :delete_container
+
   get '/images' => 'docker#images'
 end
